e6ceac3ba57f51e06dcbf64e8f9202b1aa4b6be5
Lenice1 <lenice.brissett@gmail.com>
2023-12-12 18:33:29 -0500

    Update

--- pages/home.php
+++ pages/home.php
@@ -1,25 +1,111 @@
-<?php include '../includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HRMS - Home</title>
 
-<section class="hero">
-    <h2>Welcome to HRMS</h2>
-    <p>Experience efficient and secure hospital registration services with HRMS.</p>
-    <a href="register.php" class="cta-button">Register Now</a>
-</section>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 
-<section class="features">
-    <h3>Key Features</h3>
-    <div class="feature">
-        <h4>Efficient Registration Process</h4>
-        <p>Streamlined patient registration for a quick and hassle-free experience.</p>
-    </div>
-    <div class="feature">
-        <h4>Appointment Scheduling</h4>
-        <p>Advanced appointment scheduling system for reduced wait times.</p>
-    </div>
-    <div class="feature">
-        <h4>Medical History Management</h4>
-        <p>Comprehensive medical history management accessible to patients and providers.</p>
-    </div>
-</section>
+    <!-- Custom CSS -->
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
 
-<?php include '../includes/footer.php'; ?>
+        .hero {
+            background-color: #007bff;
+            color: #ffffff;
+            padding: 100px 0;
+            text-align: center;
+        }
+
+        .cta-button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 18px;
+            font-weight: bold;
+            text-decoration: none;
+            background-color: #ffffff;
+            color: #007bff;
+            border-radius: 5px;
+        }
+
+        .cta-button:hover {
+            background-color: #e3e6ec;
+        }
+
+        .features {
+            padding: 50px 0;
+        }
+
+        .feature {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+
+    <!-- Navigation (Bootstrap Navbar) -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">HRMS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                    <!-- Add other navigation links as needed -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Hero Section -->
+    <section class="hero">
+        <div class="container">
+            <h1 class="display-4">Welcome to HRMS</h1>
+            <p class="lead">Experience efficient and secure hospital registration services with HRMS.</p>
+            <a href="register.php" class="cta-button btn btn-primary btn-lg">Register Now</a>
+        </div>
+    </section>
+
+    <!-- Features Section -->
+    <section class="features">
+        <div class="container">
+            <h2 class="text-center mb-5">Key Features</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="feature text-center">
+                        <h3 class="h4">Efficient Registration Process</h3>
+                        <p>Streamlined patient registration for a quick and hassle-free experience.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="feature text-center">
+                        <h3 class="h4">Appointment Scheduling</h3>
+                        <p>Advanced appointment scheduling system for reduced wait times.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="feature text-center">
+                        <h3 class="h4">Medical History Management</h3>
+                        <p>Comprehensive medical history management accessible to patients and providers.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Bootstrap JS (optional, for features like Navbar toggling) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
